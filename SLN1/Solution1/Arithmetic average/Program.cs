497df06e0177ec8437bee30b3433d7fabfd89795
lusine.khachatryan <lusine.khachatryan.sd@gmail.com>
2019-06-12 14:55:07 +0400

    Task 1

---
+++ SLN1/Solution1/Arithmetic average/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic_average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a three digit number->");
+            int Number = int.Parse(Console.ReadLine());
+            
+
+            int Digit1 = Number % 10;
+            Number /= 10;
+            int Digit2 = Number % 10;
+            int Digit3 = Number / 10;
+            float Average = (Digit1 + Digit2 + Digit3) / 3f;
+            Console.WriteLine("Arithmetic average is " + Average);
+            Console.ReadKey();
+        
+        }
+    }
+}
