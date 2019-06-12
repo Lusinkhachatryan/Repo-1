d1c5a22b5f1b0090ce20864d8d7eaf59e81b9526
lusine.khachatryan <lusine.khachatryan.sd@gmail.com>
2019-06-12 15:56:56 +0400

    Task3

---
+++ SLN1/Solution1/Deposit - calculation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deposit___calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Deposit Amount -> ");
+            int Amount = int.Parse(Console.ReadLine());
+            Console.Write("Interest rate ->");
+            double IntRate = double.Parse(Console.ReadLine());
+            Console.WriteLine("Total Amount will be " + Amount*(1+IntRate/100*3));
+            Console.ReadKey();
+
+        }
+    }
+}
