5e41f945946c71fb44a34605f5b5194601edaf4c
lusine.khachatryan <lusine.khachatryan.sd@gmail.com>
2019-06-14 15:17:54 +0400

    Task4+Task5

---
+++ SLN1/Solution1/Task4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("First number->");
+
+            int Num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Second number->");
+
+            int Num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("------------------");
+
+            double NumSqrt = Math.Sqrt(Num1);
+
+            Console.WriteLine("Square root of the First number is " + NumSqrt);
+
+            Console.WriteLine("------------------");
+
+            int NumDiff = Math.Abs(Num1 - Num2);
+
+            Console.WriteLine("Absolute value of the subtraction is " + NumDiff);
+
+            Console.WriteLine("------------------");
+
+            double NumPow = Math.Pow(Math.Max(Num1, Num2), 3);
+
+            Console.WriteLine("The max number raised to 3 power is " + NumPow);
+
+            Console.WriteLine("------------------");
+
+            bool IsEqual = Num1++ == --Num2;
+
+            Console.WriteLine("Postfix increment of the first number is equal to prefix decrement of the second one -> " + IsEqual);
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ SLN1/Solution1/Task5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number->");
+
+            int Number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("------------------");
+
+            Console.WriteLine("Absolute value of the number is " + Math.Abs(Number));
+
+            Console.WriteLine("------------------");
+
+            Console.WriteLine("Square root of the number is " + Math.Sqrt(Number));
+
+            Console.WriteLine("------------------");
+
+            Console.WriteLine("Sum is " + (Math.Pow(Number,3) + Math.Pow(Number/2,3)));
+
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
